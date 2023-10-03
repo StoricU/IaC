8e05a48ad4ad8042034c1ebedd59356197512ee0
John Martin Askevold <public@johnmartin.no>
2023-10-03 09:01:29 +0200

    tatt i bruk backend

---
+++ Modul06/variables.tf
@@ -0,0 +1,10 @@
+variable "rg_modul06_location" {
+  type        = string
+  description = "Lokasjon for ressursene"
+}
+
+variable "rg_modul06_name" {
+  type        = string
+  description = "Navn på resource groups"
+}
+
