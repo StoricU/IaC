58a9f9d0962470ab6ad12d49c34b07551b8ed97d
John Martin Askevold <public@johnmartin.no>
2023-10-25 13:26:17 +0200

    retry

--- Modul08/main.tf
+++ Modul08/main.tf
@@ -52,7 +52,7 @@ resource "azurerm_virtual_network" "vnet" {
 }
 
 resource "azurerm_subnet" "subnet" {
-  name                 = var.snet_name
+  name                 = "${var.snet_name}-${local.workspace_suffix}"
   resource_group_name  = azurerm_resource_group.rg.name
   virtual_network_name = azurerm_virtual_network.vnet.name
   address_prefixes     = ["10.0.2.0/24"]
@@ -76,6 +76,24 @@ resource "azurerm_network_interface" "nic" {
     private_ip_address_allocation = "Dynamic"
     public_ip_address_id          = azurerm_public_ip.pip-linux.id
 
+  }
+}
+
+resource "azurerm_network_security_group" "nsg-demo-jm" {
+  name                = "nsg-sn01-${local.workspace_suffix}"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  security_rule {
+    name                       = "Allow-Public-IP-${local.workspace_suffix}"
+    priority                   = 100
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "22"
+    source_address_prefix      = "85.89.28.144"
+    destination_address_prefix = "*"
   }
 }
 
