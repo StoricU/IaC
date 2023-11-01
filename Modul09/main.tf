834380fe58d0f96a82ef8829f5e79747d81d06c0
John Martin Askevold <public@johnmartin.no>
2023-11-01 12:39:50 +0100

    Create modul09

---
+++ Modul09/main.tf
@@ -0,0 +1,123 @@
+locals {
+  workspace_suffix = terraform.workspace == "default" ? "" : terraform.workspace
+  rg_name          = terraform.workspace == "default" ? "${var.rg_name}" : "${var.rg_name}-${local.workspace_suffix}"
+  sa_name          = terraform.workspace == "default" ? "${var.sa_name}" : "${var.sa_name}${local.workspace_suffix}"
+  #Nettsiden endrer seg basert på workspace med content variabelen:
+  content = "${var.source_content}<br><br>Workspace: ${upper(terraform.workspace)}"
+
+}
+
+resource "random_string" "random_string" {
+  length  = 6
+  special = false
+  upper   = false
+}
+
+
+resource "azurerm_resource_group" "rg" {
+  name     = local.rg_name
+  location = var.location
+}
+
+resource "azurerm_storage_account" "sa" {
+  name                     = "${lower(local.sa_name)}${random_string.random_string.result}"
+  resource_group_name      = azurerm_resource_group.rg.name
+  location                 = azurerm_resource_group.rg.location
+  account_tier             = "Standard"
+  account_replication_type = "GRS"
+
+  static_website {
+    index_document = var.index_document
+  }
+}
+
+resource "azurerm_storage_blob" "index_html" {
+  name                   = var.index_document
+  storage_account_name   = azurerm_storage_account.sa.name
+  storage_container_name = "$web"
+  type                   = "Block"
+  content_type           = "text/html"
+  source_content         = local.content
+}
+
+output "primary_web_endpoint" {
+  value = azurerm_storage_account.sa.primary_web_endpoint
+}
+
+resource "azurerm_virtual_network" "vnet" {
+  name                = "${var.vnet_name}-${local.workspace_suffix}"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+}
+
+resource "azurerm_subnet" "subnet" {
+  name                 = "${var.snet_name}-${local.workspace_suffix}"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.2.0/24"]
+}
+
+resource "azurerm_public_ip" "pip-linux" {
+  name                = "pIP-Linux-${local.workspace_suffix}"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  allocation_method   = "Static"
+}
+
+resource "azurerm_network_interface" "nic" {
+  name                = "linux-nic-${local.workspace_suffix}"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  ip_configuration {
+    name                          = "internal-${local.workspace_suffix}"
+    subnet_id                     = azurerm_subnet.subnet.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.pip-linux.id
+
+  }
+}
+
+resource "azurerm_network_security_group" "nsg-demo-jm" {
+  name                = "nsg-sn01-${local.workspace_suffix}"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  security_rule {
+    name                       = "Allow-Public-IP-${local.workspace_suffix}"
+    priority                   = 100
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "22"
+    source_address_prefix      = "85.89.28.144"
+    destination_address_prefix = "*"
+  }
+}
+
+resource "azurerm_linux_virtual_machine" "linux-demo" {
+  name                            = "VM-linux-${local.workspace_suffix}"
+  resource_group_name             = azurerm_resource_group.rg.name
+  location                        = azurerm_resource_group.rg.location
+  size                            = "Standard_F2"
+  admin_username                  = "adminuser"
+  admin_password                  = "P@ssw0rd1234!"
+  disable_password_authentication = false
+  network_interface_ids = [
+    azurerm_network_interface.nic.id,
+  ]
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "0001-com-ubuntu-server-focal"
+    sku       = "20_04-lts"
+    version   = "latest"
+  }
+}
---
+++ Modul09/providers.tf
@@ -0,0 +1,21 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "3.71.0"
+    }
+  }
+  backend "azurerm" {
+    resource_group_name  = "rg-backend-jm"
+    storage_account_name = "sabetfsjmz8c513"
+    container_name       = "tfstate"
+    key                  = "modul09.terraform.tfstate"
+
+  }
+}
+
+provider "azurerm" {
+  features {
+    }
+
+  }
