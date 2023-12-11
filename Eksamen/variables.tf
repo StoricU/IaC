12c34c6e80bcb7294104590cd5abe22288fe11f9
John Martin Askevold <public@johnmartin.no>
2023-12-11 12:15:56 +0100

    eksamen

---
+++ Eksamen/variables.tf
@@ -0,0 +1,15 @@
+variable "location" {
+  type        = string
+  description = "Lokasjon for ressursene"
+}
+
+variable "rg_name" {
+  type        = string
+  description = "Navn på resource groups"
+}
+
+variable "sa_name" { 
+  type        = string
+  description = "Navn på storage account"
+  
+}
--- Modul09/variables.tf
+++ Modul09/variables.tf
@@ -1,42 +1,42 @@
 variable "location" {
   type        = string
   description = "Lokasjon for ressursene"
-  default = "westeurope"
+  default     = "westeurope"
 }
 
 variable "rg_name" {
   type        = string
   description = "Navn på resource groups"
-  default = "rg-modul09-jm"
+  default     = "rg-modul09-jm"
 }
 
 variable "sa_name" {
   type        = string
   description = "Navn på storage account"
-  default = "sajm"
+  default     = "sajm"
 }
 
 variable "source_content" {
   type        = string
   description = "Innholdet i filen som skal lastes opp"
-  default = "<h1>My first Terraform project</h1>"
+  default     = "<h1>My first Terraform project</h1>"
 }
-  
+
 variable "index_document" {
   type        = string
   description = "Navn på index document"
-  default = "index.html"
+  default     = "index.html"
 }
 
 variable "vnet_name" {
   type        = string
   description = "Vnet name"
-  default = "vnet-jm"
-  }
+  default     = "vnet-jm"
+}
 
 variable "snet_name" {
-  type = string
+  type        = string
   description = "subnet name"
-  default = "internal"
-  
+  default     = "internal"
+
 }
